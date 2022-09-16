43eb633e7344153cce6ecb71b08cf4b6777a7618
MarcelGroeller <01618478@edu.campus02.at>
2022-09-16 19:13:50 +0200

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    //main + TAB
+    //psvm + TAB
+
+    public static void main(String[] args) {
+
+        //sout + TAB
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        //STRG+D = kopieren
+        System.out.println("Zeile 4");
+
+    }
+
+}
